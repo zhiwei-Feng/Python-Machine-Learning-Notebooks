2663d0a486e5485f2293f726216b76431d12b8fb
Adrian <zhiwei.feng1995@gmail.com>
2019-07-26 11:31:35 +0800

    day3

--- 第二章 机器学习分类算法/perceptron.py
+++ 第二章 机器学习分类算法/perceptron.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.random import seed
 
 
 class Perceptron(object):
@@ -76,6 +77,75 @@ class AdalineGD(object):
             self.cost_.append(cost)
 
         return self
+
+    def net_input(self, X):
+        """计算净输入"""
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+
+    def activation(self, X):
+        """计算线性激励函数值"""
+        return self.net_input(X)
+
+    def predict(self, X):
+        """返回类标"""
+        return np.where(self.net_input(X) >= 0.0, 1, -1)
+
+
+class AdalineSGD(object):
+    '''Adaptive Linear Neuron classifier'''
+
+    def __init__(self, eta=0.0, n_iter=10, shuffle=True, random_state=None):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.w_initialized = False
+        self.shuffle = shuffle
+        if random_state:
+            seed(random_state)
+
+    def fit(self, X, y):
+        '''拟合训练数据'''
+        self._initialize_weights(X.shape[1])
+        self.cost_ = []
+        for _ in range(self.n_iter):
+            if self.shuffle:
+                X, y = self._shuffle(X, y)
+            cost = []
+            for xi, target in zip(X, y):
+                cost.append(self._update_weights(xi, target))
+            avg_cost = sum(cost) / len(y)
+            self.cost_.append(avg_cost)
+
+        return self
+
+    def partial_fit(self, X, y):
+        """不重置权重的拟合训练数据"""
+        if not self.w_initialized:
+            self._initialize_weights(X.shape[1])
+        if y.ravel().shape[0] > 1:
+            for xi, target in zip(X, y):
+                self._update_weights(xi, target)
+        else:
+            self._update_weights(X, y)
+        return self
+
+    def _shuffle(self, X, y):
+        '''shuffle 训练数据'''
+        r = np.random.permutation(len(y))
+        return X[r], y[r]
+
+    def _initialize_weights(self, m):
+        '''初始权重为0'''
+        self.w_ = np.zeros(1 + m)
+        self.w_initialized = True
+
+    def _update_weights(self, xi, target):
+        """使用Adaline学习规则来更新权重"""
+        output = self.net_input(xi)
+        error = target - output
+        self.w_[1:] += self.eta * xi.dot(error)
+        self.w_[0] += self.eta * error
+        cost = 0.5 * error ** 2
+        return cost
 
     def net_input(self, X):
         """计算净输入"""
